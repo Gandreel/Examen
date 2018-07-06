b82fa1e81726cd10acbb5d175012f5dbf10fdd9c
aldanerking <40255376+aldanerking@users.noreply.github.com>
2018-07-06 07:18:47 -0400

    0.17

--- resources/views/analisis/index.blade.php
+++ resources/views/analisis/index.blade.php
@@ -16,38 +16,38 @@
 				<td>
 					<div class="card" style="width: 18rem;">
 						<img class="card-img-top" src="{{ asset('img/pc.jpg') }}" alt="Card image cap">
-						<div class="card-body">
+						<div class="card-body" style="text-align: center;">
 							<h5 class="card-title">Nuevos Analisis</h5>
 							<p class="card-text" style="text-align: center;">
 								Tratando de mejorar nuestro servicio de analisis, nos hemos ampliado a nuevos tipos de analisis, para su beneficio.
 							</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+							<a href="#" class="btn btn-primary"><img src="{{ asset('img/flecha.png') }}"></a>
 						</div>
 					</div>
 				</td>
 				<td>
 					<div class="card" style="width: 18rem;">
 						<img class="card-img-top" src="{{ asset('img/edificio.jpg') }}" alt="Card image cap">
-						<div class="card-body">
+						<div class="card-body" style="text-align: center;">
 							<h5 class="card-title">Nueva Sucursal</h5>
 							<p class="card-text" style="text-align: center;">
 								Para poder dar mayor comodidad a nuestros clientes se ha abrierto una nueva sucursal
 								en la V region de Valparaiso.
 							</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+							<a href="#" class="btn btn-primary"><img src="{{ asset('img/flecha.png') }}"></a>
 						</div>
 					</div>
 				</td>
 				<td>
 					<div class="card" style="width: 18rem;">
 						<img class="card-img-top" src="{{ asset('img/oficina.jpg') }}" alt="Card image cap">
-						<div class="card-body">
+						<div class="card-body" style="text-align: center;">
 							<h5 class="card-title">Servicio Mejorado</h5>
 							<p class="card-text" style="text-align: center;">
 								Tratando de ayudar a nuestros clientes, hemos acatualizados nuestros servicios
 								para poder realizarse nuestra pagina web.
 							</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
+							<a href="#" class="btn btn-primary"><img src="{{ asset('img/flecha.png') }}"></a>
 						</div>
 					</div>
 				</td>
