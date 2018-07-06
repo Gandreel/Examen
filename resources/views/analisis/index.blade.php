e87d17bae686674c0843a113f2d1b998a2930b7d
aldanerking <40255376+aldanerking@users.noreply.github.com>
2018-07-06 02:30:24 -0400

    0.13

---
+++ resources/views/analisis/index.blade.php
@@ -0,0 +1,9 @@
+@extends('analisis.layout.template')
+@section('title', 'Inicio')
+@section('contenido')
+	
+	 <div class="index-titulo">
+	 	<h3>Noticias</h3>
+	 </div>  
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::get('registro','ParticularController@RegistroParticular');
+Route::get('inicio','PaginaController@index');
