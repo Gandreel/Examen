cb507304b24019bf13d853ebcd71f92dbd50ed51
Shaqattack32 <Shaqattack32@users.noreply.github.com>
2018-07-11 00:20:32 -0400

    0.33

--- resources/views/analisis/BusquedaMuestra.blade.php
+++ resources/views/analisis/BusquedaMuestra.blade.php
@@ -1,7 +1,31 @@
 @extends('analisis.layout.template')
 @section('title', 'Busqueda Muestra')
 @section('contenido')
-	
-	hola mundo    
 
+<div class="muestra_titulo">
+  <h3>Busqueda Muestras</h3>
+	<h5 color= "gray">Escribe el codigo de la muestra a buscar</h5>
+</div>
+<div class="cuerpo-Listar">
+	<table>
+			<tr>
+				<td><input type="text" placeholder="Codigo de muestra" name="txtCodigoMuestra"></td>
+				<button type="submit" name="boton" class="boton2" value="{{$muestra->idAnalisisMuestras}}">Buscar</button>
+			</tr>
+			<thead>
+				<tr>
+					<th>Codigo de la muestra</th>
+					<th>Estado</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($datos as $muestra)
+				<tr>
+					<td>{{ $muestra->idAnalisisMuestras}}</td>
+					<td>{{ $muestra->estado}}</td>
+				</tr>
+				@endforeach
+			</tbody>
+	</table>
+</div>
 @endsection
