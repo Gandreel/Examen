80e141ef71ad3cf94b8daf9a674bf11cb16e3e1d
aldanerking <40255376+aldanerking@users.noreply.github.com>
2018-07-11 09:19:35 -0400

    0.39

--- resources/views/analisis/RegistroMuestra.blade.php
+++ resources/views/analisis/RegistroMuestra.blade.php
@@ -5,7 +5,6 @@
     <h3>Registro de las Muestras</h3>
 </div>
 <div class="row">
-
     <div class="alert alert-success col-5 cont">
         @foreach($datos as $muestra)
         Codigo del Cliente: {{$muestra->idAnalisisMuestras}}
@@ -17,32 +16,33 @@
         Codigo de la Muestra: {{$muestra->Particular_codigoParticular}}
         @endforeach
     </div>
-    
-    <div class="col-12 dav">
-        <table class="tablita">
-        <thead>
-            <tr>
-                <th>Tipo de Analisis</th>
-                <th>PPM de muestra</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Mitocondrias</td>
-                <td><input class="text2" type="text" name="txtMicrotoxinas"/></td>
-            </tr>
-            <tr>
-                <td>Metales Pesados</td>
-                <td><input class="text2" type="text" name="txtMetalesPesados"/></td>
-            </tr>
-            <tr>
-                <td>Pesticidas</td>
-                <td><input class="text2" type="text" name="txtpestisidas"/></td>
-            </tr>
-        </tbody>
-    </table>
-    </div>
-    <br>
-    <input class="boton3" type="submit" value="Guardar Analisis" name="btnGuardarAnalisis" />
+    <form action="{{URL::to('create')}}" method="post">
+        <div class="col-12 dav">
+            <table class="tablita">
+                <thead>
+                    <tr>
+                        <th>Tipo de Analisis</th>
+                        <th>PPM de muestra</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mitocondrias</td>
+                        <td><input class="text2" type="text" name="txtMicrotoxinas"/></td>
+                    </tr>
+                    <tr>
+                        <td>Metales Pesados</td>
+                        <td><input class="text2" type="text" name="txtMetalesPesados"/></td>
+                    </tr>
+                    <tr>
+                        <td>Pesticidas</td>
+                        <td><input class="text2" type="text" name="txtpestisidas"/></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <input class="boton3" type="submit" value="Guardar Analisis" name="boton"/>
+        <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+    </form>
 </div>
 @endsection
